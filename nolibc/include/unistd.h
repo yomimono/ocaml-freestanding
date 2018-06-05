adb0c83ecb90f545a3e23dac1ede0cf6e42a3cd1
Mindy <meetup@yomimono.org>
2018-06-05 17:05:53 -0500

    define ssize_t only if it's unknown

--- nolibc/include/unistd.h
+++ nolibc/include/unistd.h
@@ -1,6 +1,15 @@
 #ifndef _UNISTD_H
 #define _UNISTD_H
 
+#if (!defined __DEFINED_ssize_t)
+typedef int ssize_t;
+#define __DEFINED_ssize_t
+#endif
+#if (!defined __DEFINED_off_t)
+typedef int off_t;
+#define __DEFINED_off_t
+#endif
+
 int chdir(const char *);
 int close(int);
 char *getcwd(char *, size_t);
@@ -8,9 +17,7 @@ typedef int pid_t;
 pid_t getpid(void);
 pid_t getppid(void);
 int isatty(int);
-typedef int off_t;
 off_t lseek(int, off_t, int); /* SEEK_ */
-typedef int ssize_t;
 ssize_t read(int, void *, size_t);
 ssize_t write(int, const void *, size_t);
 ssize_t readlink(const char *, char *, size_t);
